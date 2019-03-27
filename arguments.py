7894d9f2bdeefd192bd3e3a224b45d74c1db364a
bubblekin <conradmark75@yahoo.com>
2019-03-26 20:01:07 -0500

    changed range for block size

--- arguments.py
+++ arguments.py
@@ -9,7 +9,7 @@ parser.add_argument("-f", dest="file", required=True,
 parser.add_argument("-s", dest="cache_size", type=int,
                     required=True, metavar="[1-8192]", help="Cache size in KB (1KB to 8MB)")
 parser.add_argument("-b", dest="block_size", type=int,
-                    choices=range(4, 64), metavar="[4-64]", required=True, help="Block size in bytes (4 bytes to 64 bytes)")
+                    choices=range(4, 65), metavar="[4-64]", required=True, help="Block size in bytes (4 bytes to 64 bytes)")
 parser.add_argument("-a", dest="associativity", type=int,
                     required=True, help="Associativity (1, 2, 3, 4, 8, 16)", choices=[1, 2, 3, 4, 8, 16])
 parser.add_argument("-r", dest="replacement_policy", required=True,
